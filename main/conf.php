5dd82fdcfd017e03225b2ea0578c8f3ab6e7d852
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-10-26 22:05:39 -0500

    cm6
    Cambio en aarchivo conf.php del repositorio ubuntu mas no de la carpeta maraciones

--- main/conf.php
+++ main/conf.php
@@ -1,7 +1,7 @@
 <?php 
 try {
-   // $conn = new PDO("sqlsrv:Server=10.10.10.24,1433;Database=OnlyControl;LoginTimeout=60", 'ocaccess', 'oc1976');	
-    $conn = new PDO("sqlsrv:Server=10.10.10.24,1433;Database=OnlyControl", "ocaccess","oc1976");
+   // $conn = new PDO("sqlsrv:Server=10.10.10.53,8765;Database=OnlyControl;LoginTimeout=60", 'ocaccess', 'oc1976');	
+    $conn = new PDO("sqlsrv:Server=10.10.10.53,8765;Database=GADP_AZUAY", "bbravo","pasantebb");
     $conn->setAttribute(PDO::SQLSRV_ATTR_QUERY_TIMEOUT, 5);
     $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     #$conn = conectar_db ();
