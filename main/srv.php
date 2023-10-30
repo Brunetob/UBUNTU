c1e5a988381c87f08d7cce0c1adde43eb3cb84e0
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-10-30 16:20:18 -0500

    cm21

--- main/srv.php
+++ main/srv.php
@@ -12,9 +12,12 @@ require_once('conf.php');
                 while ($row = pg_fetch_assoc($result)) {
                     $nombre = $row['name_related'];
                     echo $nombre;
+                    exit();
                 }
             } else {
+                http_response_code(500);
                 echo "ERROR";
+                exit();
             }
         }
     
@@ -23,7 +26,7 @@ require_once('conf.php');
             $san_cedula = filter_var($cedula, FILTER_SANITIZE_NUMBER_INT);
             $ip = strval($_SERVER['REMOTE_ADDR']);
             $equipo = strval(gethostbyaddr($_SERVER['REMOTE_ADDR']));
-            $nombre = ''; // Variable para almacenar el nombre
+            $nombre = '';
     
             $sql = "SELECT name_related FROM hr_employee WHERE identification_id = '$san_cedula'";
             $result = pg_query($dbconn, $sql);
@@ -34,14 +37,19 @@ require_once('conf.php');
             }
     
             if ($nombre !== '') {
-                try {
-                    $sql = "INSERT INTO gpa_devicedata (cedula, nombre, fecha, hora, ip, equipo) VALUES ('$san_cedula', '$nombre', CURRENT_DATE, CURRENT_TIME, '$ip', '$equipo')";
-                    $conn->query($sql);
-                } catch (Exception $e) {
-                    echo "ERROR";
+                $sql = "INSERT INTO gpa_devicedata (cedula, nombre, fecha, hora, ip, equipo) VALUES ('$san_cedula', '$nombre', CURRENT_DATE, CURRENT_TIME, '$ip', '$equipo')";
+                if ($conn->query($sql)) {
+                    echo "MARCACION_EXITOSA";
+                    exit();
+                } else {
+                    http_response_code(500);
+                    echo "ERROR_EN_INSERCION";
+                    exit();
                 }
             } else {
-                echo "Empleado no encontrado"; // Si no se encuentra el empleado
+                http_response_code(404);
+                echo "EMPLEADO_NO_ENCONTRADO";
+                exit();
             }
         }
     }
