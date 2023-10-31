638bb5e8435397fc065980ee14bd06e913e302f2
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-10-31 13:11:18 -0500

    cm22

--- main/srv.php
+++ main/srv.php
@@ -20,14 +20,14 @@ require_once('conf.php');
                 exit();
             }
         }
-    
+
         if (isset($_POST['cedula']) && isset($_POST['marcar'])) {
             $cedula = $_POST['cedula'];
             $san_cedula = filter_var($cedula, FILTER_SANITIZE_NUMBER_INT);
             $ip = strval($_SERVER['REMOTE_ADDR']);
             $equipo = strval(gethostbyaddr($_SERVER['REMOTE_ADDR']));
             $nombre = '';
-    
+
             $sql = "SELECT name_related FROM hr_employee WHERE identification_id = '$san_cedula'";
             $result = pg_query($dbconn, $sql);
             if ($result) {
@@ -35,7 +35,7 @@ require_once('conf.php');
                     $nombre = $row['name_related'];
                 }
             }
-    
+
             if ($nombre !== '') {
                 $sql = "INSERT INTO gpa_devicedata (cedula, nombre, fecha, hora, ip, equipo) VALUES ('$san_cedula', '$nombre', CURRENT_DATE, CURRENT_TIME, '$ip', '$equipo')";
                 if ($conn->query($sql)) {
