2ed471f900895d6387650ea6818bc2b9209c71b6
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-11-06 13:10:26 -0500

    cm31

--- new/srv.php
+++ new/srv.php
@@ -5,11 +5,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['cedula']) && isset($_POST['check'])) {
         $cedula = $_POST['cedula'];
         $san_cedula = filter_var($cedula, FILTER_SANITIZE_NUMBER_INT);
-        $sql = "SELECT name_related FROM hr_employee WHERE identification_id = $1";
-        $result = pg_query_params($dbconn, $sql, array($san_cedula));
+        $sql = "SELECT name_related FROM hr_employee WHERE identification_id = :san_cedula";
 
-        if ($result) {
-            $row = pg_fetch_assoc($result);
+        try {
+            $stmt = $dbconn->prepare($sql);
+            $stmt->bindParam(':san_cedula', $san_cedula);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
             if ($row) {
                 echo $row['name_related'];
                 exit();
@@ -18,9 +21,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo "EMPLEADO_NO_ENCONTRADO";
                 exit();
             }
-        } else {
+        } catch (PDOException $e) {
             http_response_code(500);
-            echo "ERROR";
+            echo "ERROR: " . $e->getMessage();
             exit();
         }
     }
@@ -28,11 +31,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (isset($_POST['cedula']) && isset($_POST['marcar'])) {
         $cedula = $_POST['cedula'];
         $san_cedula = filter_var($cedula, FILTER_SANITIZE_NUMBER_INT);
-        $sql = "SELECT name_related FROM hr_employee WHERE identification_id = $1";
-        $result = pg_query_params($dbconn, $sql, array($san_cedula));
-
-        if ($result) {
-            $row = pg_fetch_assoc($result);
+        $sql = "SELECT name_related FROM hr_employee WHERE identification_id = :san_cedula";
+    
+        try {
+            $stmt = $dbconn->prepare($sql);
+            $stmt->bindParam(':san_cedula', $san_cedula);
+            $stmt->execute();
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    
             if ($row) {
                 $nombre = $row['name_related'];
                 $ip = strval($_SERVER['REMOTE_ADDR']);
@@ -41,10 +47,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $hora = date("H:i:s");
                 $fecha_hora = date("Y-m-d H:i:s");
                 $hora_varchar = date("H:i");
-
-                $sql_insert = "INSERT INTO gpa_devicedata (usuario_cedula, usuario_name, fecha, hora, ip, fecha_hora, hora_varchar) VALUES ('$san_cedula', '$nombre', '$fecha', '$hora', '$ip', '$fecha_hora', '$hora_varchar')";
-
-                if (pg_query($dbconn, $sql_insert)) {
+    
+                $sql_insert = "INSERT INTO gpa_devicedata (usuario_cedula, usuario_name, fecha, hora, ip, fecha_hora, hora_varchar) 
+                               VALUES (:san_cedula, :nombre, :fecha, :hora, :ip, :fecha_hora, :hora_varchar)";
+    
+                $stmt = $dbconn->prepare($sql_insert);
+                $stmt->bindParam(':san_cedula', $san_cedula);
+                $stmt->bindParam(':nombre', $nombre);
+                $stmt->bindParam(':fecha', $fecha);
+                $stmt->bindParam(':hora', $hora);
+                $stmt->bindParam(':ip', $ip);
+                $stmt->bindParam(':fecha_hora', $fecha_hora);
+                $stmt->bindParam(':hora_varchar', $hora_varchar);
+    
+                if ($stmt->execute()) {
                     echo "MARCACION_EXITOSA";
                     exit();
                 } else {
@@ -57,11 +73,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo "EMPLEADO_NO_ENCONTRADO";
                 exit();
             }
-        } else {
+        } catch (PDOException $e) {
             http_response_code(500);
-            echo "ERROR";
+            echo "ERROR: " . $e->getMessage();
             exit();
         }
-    }
+    }    
 }
 ?>
