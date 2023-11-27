73523e8c616631055ca9b61411432216e7e23094
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-11-27 13:13:23 -0500

    cm79.7

--- new/js/prueba.js
+++ new/js/prueba.js
@@ -64,37 +64,12 @@ $(function() {
         });
     }
 
-    /*function showErrorAlert() {
+    function showErrorAlert() {
         Swal.fire({
             title: 'Algo salió mal.',
-            text: 'Error en la solicitud.',
+            text: 'Error en la solicitud ERROR 500',
             type: 'error'
         });
-    }*/
-    function showErrorAlert(data) {
-        switch (data) {
-            case 'EMPLEADO_NO_REGISTRADO':
-                Swal.fire({
-                    title: 'Algo salió mal.',
-                    text: 'Funcionario no registrado.',
-                    type: 'error'
-                });
-                break;
-            case 'ERROR':
-                Swal.fire({
-                    title: 'Algo salió mal.',
-                    text: 'Error en la solicitud.',
-                    type: 'error'
-                });
-                break;
-            default:
-                Swal.fire({
-                    title: 'Algo salió mal.',
-                    text: 'Error en la solicitud.',
-                    type: 'error'
-                });
-                break;
-        }
     }
 
     function clearFormFields() {
