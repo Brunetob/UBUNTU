8226827e2738982f7b5f9d47df27dd0f6749dbd7
Brunetob <116246756+Brunetob@users.noreply.github.com>
2023-11-28 10:57:47 -0500

    cm81
    Creación de documentación

--- new/js/prueba.js
+++ new/js/prueba.js
@@ -1,54 +1,110 @@
 $(function() {
+    // Selecciona el formulario con el ID 'marcacionform' y añade un controlador de eventos para el evento 'submit'
     $('#marcacionform').submit(function(e) {
+        // Previene la acción por defecto del evento 'submit', que es enviar el formulario
         e.preventDefault();
+
+        // Obtiene el valor del campo de entrada con el ID 'cedula'
         let cedula = $('#cedula').val();
-        $.post('test.php', { cedula: cedula, marcar: true }, function(data) { // aquí va srv.php
+
+        // Realiza una petición POST a 'test.php' con los datos del formulario
+        // El objeto { cedula: cedula, marcar: true } se envía como cuerpo de la petición
+        // 'test.php' debe ser reemplazado por 'srv.php' según el comentario existente
+        $.post('test.php', { cedula: cedula, marcar: true }, function(data) {
+            // Esta función se ejecuta cuando la petición es exitosa
+            // 'data' contiene la respuesta del servidor
+            // La función 'handleResponse' se encarga de manejar la respuesta
             handleResponse(data);
         }).fail(function() {
+            // Esta función se ejecuta si la petición falla
+            // La función 'showErrorAlert' se encarga de mostrar una alerta de error
             showErrorAlert();
         });
     });
 
+    // Selecciona el campo de entrada con el ID 'cedula' y añade un controlador de eventos para el evento 'input'
     $('#cedula').on('input', function() {
+        // Obtiene el valor del campo de entrada con el ID 'cedula'
         let cedula = $('#cedula').val();
+
+        // Si el campo 'cedula' está vacío, limpia el campo 'usuario'
         if (cedula.length === 0) {
             $('#usuario').val('');
-        } else if (cedula.length === 10) {
-            $.post('test.php', { cedula: cedula, check: true }, function(data) { // aquí va srv.php
+        } 
+        // Si el campo 'cedula' tiene exactamente 10 caracteres
+        else if (cedula.length === 10) {
+            // Realiza una petición POST a 'test.php' con los datos del formulario
+            // El objeto { cedula: cedula, check: true } se envía como cuerpo de la petición
+            // 'test.php' debe ser reemplazado por 'srv.php' según el comentario existente
+            $.post('test.php', { cedula: cedula, check: true }, function(data) {
+                // Esta función se ejecuta cuando la petición es exitosa
+                // 'data' contiene la respuesta del servidor
+                // La función 'handleCheckResponse' se encarga de manejar la respuesta
                 handleCheckResponse(data);
             }).fail(function() {
+                // Esta función se ejecuta si la petición falla
+                // La función 'showErrorAlert' se encarga de mostrar una alerta de error
                 showErrorAlert();
             });
         }
     });
 
+    // Esta función maneja la respuesta del servidor a la petición de verificación del empleado
     function handleCheckResponse(data) {
+        // Si la respuesta del servidor es 'EMPLEADO_NO_REGISTRADO' (ignorando espacios al principio y al final)
         if (data.trim() === 'EMPLEADO_NO_REGISTRADO') {
+            // Establece el valor del campo de entrada con el ID 'usuario' a 'Funcionario no registrado'
             $('#usuario').val('Funcionario no registrado');
         } else {
+            // Si la respuesta del servidor es cualquier otra cosa
+            // Establece el valor del campo de entrada con el ID 'usuario' a la respuesta del servidor
             $('#usuario').val(data);
+            // Llama a la función 'markAttendance' para marcar la asistencia del empleado
             markAttendance();
         }
     }
 
+    // Esta función se encarga de marcar la asistencia del empleado
     function markAttendance() {
+        // Obtiene el valor del campo de entrada con el ID 'cedula'
         let cedula = $('#cedula').val();
+        // Crea un nuevo objeto Date para obtener la hora actual
         let now = new Date();
+        // Formatea la hora actual en el formato 'horas:minutos:segundos'
         let formattedTime = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
-        $.post('test.php', { cedula: cedula, hora: formattedTime, marcar: true }, function(data) { // aquí va srv.php
+        // Realiza una petición POST a 'test.php' con los datos del formulario
+        // El objeto { cedula: cedula, hora: formattedTime, marcar: true } se envía como cuerpo de la petición
+        // 'test.php' debe ser reemplazado por 'srv.php' según el comentario existente
+        $.post('test.php', { cedula: cedula, hora: formattedTime, marcar: true }, function(data) {
+            // Esta función se ejecuta cuando la petición es exitosa
+            // 'data' contiene la respuesta del servidor
+            // La función 'handleResponse' se encarga de manejar la respuesta
             handleResponse(data);
         }).fail(function() {
+            // Esta función se ejecuta si la petición falla
+            // La función 'showErrorAlert' se encarga de mostrar una alerta de error
             showErrorAlert();
         });
     }
 
+    // Esta función maneja la respuesta del servidor a la petición de marcación de asistencia
     function handleResponse(data) {
+        // Si la respuesta del servidor incluye la palabra 'ERROR'
         if (data.includes('ERROR')) {
+            // Llama a la función 'showErrorAlert' para mostrar una alerta de error
             showErrorAlert();
-        } else if (data.includes('EMPLEADO_NO_REGISTRADO')) {
+        } 
+        // Si la respuesta del servidor incluye la frase 'EMPLEADO_NO_REGISTRADO'
+        else if (data.includes('EMPLEADO_NO_REGISTRADO')) {
+            // Establece el valor del campo de entrada con el ID 'usuario' a 'Funcionario no registrado'
             $('#usuario').val('Funcionario no registrado');
-        } else {
+        } 
+        // Si la respuesta del servidor no incluye ninguna de las anteriores
+        else {
+            // Llama a la función 'showSuccessAlert' con la respuesta del servidor como argumento
+            // para mostrar una alerta de éxito con el mensaje de la respuesta
             showSuccessAlert(data);
+            // Llama a la función 'clearFormFields' para limpiar los campos del formulario
             clearFormFields();
         }
     }
@@ -82,9 +138,18 @@ $(function() {
         setInterval(actualizarReloj, 1000);
     });
 
+    /**
+     * Updates the digital clock display with the current time.
+     */
     function actualizarReloj() {
         let date = new Date(Date.now() - 7000);
         $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
+
+        /**
+         * Adds a leading zero to a number if it is less than 10.
+         * @param {number} x - The number to add a leading zero to.
+         * @returns {string} - The number with a leading zero if necessary.
+         */
         function addZero(x) {
             if (x < 10) {
                 return x = '0' + x;
@@ -92,6 +157,7 @@ $(function() {
                 return x;
             }
         }
+
         let h = addZero(date.getHours());
         let m = addZero(date.getMinutes());
         let s = addZero(date.getSeconds());
--- new/js/validation.js
+++ new/js/validation.js
@@ -1,13 +1,28 @@
-$(function() {
-    // Cuando el formulario con id 'marcacionform' es enviado
-    $('#marcacionform').submit(function(e) {
-        e.preventDefault(); // Evita que el formulario se envíe de manera convencional
+/**
+ * Este script maneja la validación y envío de formularios para un formulario específico.
+ * Incluye funciones para manejar el envío del formulario, la validación de entradas y mostrar mensajes de éxito/error.
+ * El script también incluye funciones para actualizar el reloj digital y prevenir ciertos eventos de teclado.
+ *
+ * @fileoverview Script de validación y envío para un formulario específico.
+ * @version 1.0
+ */
 
-        // Obtiene el valor del campo 'cedula'
+// FILEPATH: /C:/BRUNO/PROGRAMAS_MIOS/GITHUB_MIA/UBUNTU/new/js/validation.js
+
+$(function() {
+    /**
+     * Maneja el envío del formulario cuando se envía el formulario con el id 'marcacionform'.
+     * Evita que el formulario se envíe convencionalmente.
+     * Obtiene el valor del campo 'cedula'.
+     * Envía una solicitud POST a 'srv.php' con los datos del formulario.
+     * Muestra mensajes de éxito/error según la respuesta.
+     */
+    $('#marcacionform').submit(function(e) {
+        e.preventDefault();
+
         let cedula = $('#cedula').val();
 
-        // Realiza una solicitud POST a srv.php con los datos del formulario
-        $.post('srv.php', { cedula: cedula, marcar: true }, function(data) { // Aquí es srv.php
+        $.post('srv.php', { cedula: cedula, marcar: true }, function(data) {
             if (data.includes('ERROR')) {
                 showErrorAlert();
             } else if (data.includes('EMPLEADO_NO_ENCONTRADO')) {
@@ -21,34 +36,44 @@ $(function() {
         });
     });
 
+    /**
+     * Maneja el evento de cambio de entrada para el campo 'cedula'.
+     * Obtiene el valor del campo 'cedula'.
+     * Si la longitud de la cedula es 0, borra el campo 'usuario'.
+     * Si la longitud de la cedula es 10, envía una solicitud POST a 'srv.php' con la cedula.
+     * Muestra mensajes de éxito/error según la respuesta.
+     * Llama a la función 'markAttendance' para registrar la asistencia con la hora actual.
+     */
     $('#cedula').on('input', function() {
-        // Cada vez que el campo 'cedula' cambia
         let cedula = $('#cedula').val();
         if (cedula.length === 0) {
-            $('#usuario').val(''); // Limpiar el campo de usuario si la cédula se borra
-            // Si la longitud de la cedula es igual a 10, realiza una solicitud POST a srv.php
+            $('#usuario').val('');
         } else if (cedula.length === 10) {
-            $.post('srv.php', { cedula: cedula, check: true }, function(data) { // Aquí es srv.php
+            $.post('srv.php', { cedula: cedula, check: true }, function(data) {
                 if (data.trim() === 'EMPLEADO_NO_ENCONTRADO') {
-                    $('#usuario').val('Funcionario no existe'); // Mostrar mensaje cuando el empleado no existe
+                    $('#usuario').val('Funcionario no existe');
                 } else {
                     $('#usuario').val(data);
-                    markAttendance(cedula); // Llama a una función para registrar la asistencia con la hora actual
+                    markAttendance(cedula);
                 }
             }).fail(function() {
-                console.log('Error en la solicitud.  El funcionario no se encuentra registrado en la base de datos');
-                $('#usuario').val('Funcionario no existe'); // Mostrar mensaje cuando el empleado no existe
+                console.log('Error en la solicitud. El funcionario no se encuentra registrado en la base de datos');
+                $('#usuario').val('Funcionario no existe');
             });
         }
     });
 
-    //Función para obtener la hora actual
+    /**
+     * Registra la asistencia con la hora actual.
+     * Obtiene la hora actual en formato HH:MM:SS.
+     * Envía una solicitud POST a 'srv.php' con la cedula y la hora formateada.
+     * Muestra mensajes de éxito/error según la respuesta.
+     */
     function markAttendance(cedula) {
-        // Obtiene la hora actual en formato HH:MM:SS
         let now = new Date();
         let formattedTime = now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
         
-        $.post('srv.php', { cedula: cedula, hora: formattedTime, marcar: true }, function(data) { // Aquí es srv.php
+        $.post('srv.php', { cedula: cedula, hora: formattedTime, marcar: true }, function(data) {
             if (data.trim() === 'ERROR') {
                 showErrorAlert();
             } else if (data.trim() === 'EMPLEADO_NO_ENCONTRADO') {
@@ -62,7 +87,10 @@ $(function() {
         });
     }
 
-    // Función para mostrar un mensaje de error con SweetAlert
+    /**
+     * Muestra un mensaje de error utilizando SweetAlert.
+     * @param {string} errorMsg - El mensaje de error a mostrar.
+     */
     function showErrorAlert(errorMsg) {
         Swal.fire({
             title: 'Algo salió mal.',
@@ -71,29 +99,42 @@ $(function() {
         });
     }
 
-    // Función para mostrar un mensaje de marcación exitosa con SweetAlert
+    /**
+     * Muestra un mensaje de éxito utilizando SweetAlert.
+     * Imprime la respuesta del servidor en la consola.
+     * Borra los campos del formulario después de mostrar el mensaje de éxito.
+     * @param {string} data - Los datos de respuesta del servidor.
+     */
     function showSuccessAlert(data) {
-
-        console.log(data); // Imprime la respuesta del servidor en la consola
-        Swal.fire({//fuera del try
+        console.log(data);
+        Swal.fire({
             title: 'Marcación Exitosa',
             html: data,
             type: 'success'
         }).then(() => {
             clearFormFields();
-        });//fuera del try
+        });
     }
 
-    // Función para borrar los valores del formulario
+    /**
+     * Borra los valores de los campos del formulario.
+     */
     function clearFormFields() {
         $('#cedula').val("");
         $('#usuario').val("");
     }
 
+    /**
+     * Actualiza el reloj digital cada segundo.
+     */
     $(document).ready(function() {
         actualizarReloj();
         setInterval(actualizarReloj, 1000);
     });
+
+    /**
+     * Updates the digital clock with the current time.
+     */
     function actualizarReloj() {
         let date = new Date(Date.now() - 7000);
         $('.digital-clock').css({'color': '#fff', 'text-shadow': '0 0 6px #ff0'});
@@ -110,26 +151,27 @@ $(function() {
         $('.digital-clock').text(h + ':' + m + ':' + s)
     }
 
-    // No permitir el Enter
+    /**
+     * Prevents the Enter key from being pressed inside a form.
+     * @param {object} event - The keydown event object.
+     * @returns {boolean} - Returns false to prevent the Enter key event.
+     */
     $(document).on("keydown", "form", function(event) { 
         return event.key != "Enter";
     });
 
+    /**
+     * Validates the input to allow only numeric characters for the 'cedula' field.
+     * Displays an error message if a non-numeric character is entered.
+     * @param {object} e - The keypress event object.
+     * @returns {boolean} - Returns false to prevent non-numeric characters from being entered.
+     */
     $(document).ready(function () {
-        // Llamado cuando se presiona una tecla en el cuadro de texto
         $("#cedula").keypress(function (e) {   
             if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-                // Mostrar mensaje de error
                 $("#errmsg").html("Solo números").show().fadeOut("slow");
                 return false;
             }
         });
     });
-
-    /*$(document).ready(function(){
-        // Deshabilitar cortar, copiar y pegar en el campo de cédula
-        $('#cedula').on("cut copy paste", function(e) {
-            e.preventDefault();
-        });
-    });*/
 });
